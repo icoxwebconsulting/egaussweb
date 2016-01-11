b8a703c8cf5e0d9fb9202e7812b4e953f6e98892
Dioleisys <fontelag@gmail.com>
2016-01-10 21:23:22 -0500

    Creando gestion de la pagina Donde Estamos

--- src/BackendBundle/Controller/BackendController.php
+++ src/BackendBundle/Controller/BackendController.php
@@ -2,7 +2,9 @@
 
 namespace BackendBundle\Controller;
 
+use BackendBundle\Entity\DondeEstamos;
 use BackendBundle\Entity\MisionVision;
+use BackendBundle\Form\DondeEstamosType;
 use BackendBundle\Form\MisionVisionType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -70,7 +72,7 @@ class BackendController extends Controller {
      * @Route("/mision_vision_new", name="_mision_vision_new")
      * @Template()
      */
-    public function newAction() {
+    public function newMisionVisionAction() {
         $document = new MisionVision();
         $form = $this->createForm(MisionVisionType::class, $document);
         return $this->render("BackendBundle:MisionVision:new.html.twig", array(
@@ -79,6 +81,62 @@ class BackendController extends Controller {
         ));
     }
 
+
+
+    /*
+     * Donde Estamos
+     */
+    /**
+     * @Route("/donde_estamos", name="_donde_estamos")
+     * @Template()
+     */
+    public function dondeEstamosAction() {
+
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository("BackendBundle:DondeEstamos");
+        $entities = $repo->findAll();
+
+        if(count($entities)>0)
+            return $this->render("BackendBundle:DondeEstamos:dondeEstamos.html.twig", array("entity" => $entities[0]));
+        else
+            return $this->render("BackendBundle:DondeEstamos:dondeEstamos.html.twig");
+
+    }
+
+    /**
+     * @Route("/donde_estamos_create", name="_donde_estamos_create")
+     * @Method("POST")
+     */
+    public function dondeEstamosCreateAction(Request $request) {
+
+        $document = new DondeEstamos();
+        $form = $this->createForm(DondeEstamosType::class, $document);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($document);
+            $em->flush();
+
+            return $this->render("BackendBundle:DondeEstamos:dondeEstamos.html.twig", array("entity" => $document));
+        }
+
+        return $this->render("::error.html.twig");
+
+    }
+
+    /**
+     * @Route("/donde_estamos_new", name="_donde_estamos_new")
+     * @Template()
+     */
+    public function newDondeEstamosAction() {
+        $document = new DondeEstamos();
+        $form = $this->createForm(DondeEstamosType::class, $document);
+        return $this->render("BackendBundle:DondeEstamos:new.html.twig", array(
+            'entity' => $document,
+            'form' => $form->createView()
+        ));
+    }
 
 
 }
---
+++ src/BackendBundle/Entity/DondeEstamos.php
@@ -0,0 +1,335 @@
+<?php
+
+namespace BackendBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * DondeEstamos
+ * @ORM\HasLifecycleCallbacks
+ * @ORM\Table(name="donde_estamos")
+ * @ORM\Entity(repositoryClass="BackendBundle\Repository\DondeEstamosRepository")
+ */
+class DondeEstamos
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=false)
+     */
+    protected $titulo;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=false)
+     */
+    protected $urlvideo;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=false)
+     */
+    protected $latitud;
+    /**
+     * @ORM\Column(type="string", length=100, nullable=false)
+     */
+    protected $longitud;
+
+    /**
+     * @var string $path
+     * @ORM\Column(name="path", type="string", nullable=true)
+     */
+    protected $path;
+    protected $temp;
+
+    /**
+     * @Assert\File(maxSize ="4M")
+     * @Assert\NotBlank
+     */
+    protected $foto;
+
+
+
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $direccion;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets foto.
+     *
+     * @param UploadedFile $foto
+     */
+    public function setFoto(UploadedFile $foto = null) {
+        $this->foto = $foto;
+        // check if we have an old foto path
+        if (isset($this->path)) {
+            // store the old name to delete after the update
+            $this->temp = $this->path;
+            $this->path = null;
+        } else {
+            $this->path = 'initial';
+        }
+    }
+
+    protected function getUploadRootDir() {
+        return __DIR__.'/../../../web/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir() {
+        return 'uploads/misionvision/' . $this->id;
+    }
+
+    public function getAbsolutePath() {
+        return null === $this->path ? null : $this->getUploadRootDir() . '/' . $this->path;
+    }
+
+    public function getWebPath() {
+        return null === $this->path ? null : $this->getUploadDir() . '/' . $this->path;
+    }
+
+    public function getAbsoluteCropPath() {
+        return null === $this->path ? null : $this->getUploadRootDir() . '/crop_' . $this->path;
+    }
+
+    public function getThumbnail() {
+        return null === $this->path ? null : $this->getUploadDir() . '/crop_' . $this->path;
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload() {
+        if (NULL !== $this->foto) {
+            $fotoname = sha1(uniqid(mt_rand(), true));
+            $this->path = $fotoname . '.' . $this->getFoto()->guessExtension();
+        }
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload() {
+        if (null === $this->getFoto()) {
+            return;
+        }
+        if (isset($this->temp)) {
+            try {
+                unlink($this->getUploadRootDir() . '/' . $this->temp);
+            } catch (\Exception $e) {
+                //nada
+            }
+            $this->temp = null;
+        }
+        $this->getFoto()->move($this->getUploadRootDir(), $this->path);
+        $this->foto = null;
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload() {
+        if ($foto = $this->getAbsolutePath()) {
+            try {
+                unlink($foto);
+            } catch (\Exception $e) {
+                //ignore
+            }
+        }
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     * @return Document
+     */
+    public function setPath($path) {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string
+     */
+    public function getPath() {
+        return $this->path;
+    }
+
+    /**
+     * Get foto
+     *
+     * @return UploadedFile
+     */
+    public function getFoto() {
+        return $this->foto;
+    }
+
+    /**
+     * Set titulo
+     *
+     * @param string $titulo
+     *
+     * @return Pagina
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get titulo
+     *
+     * @return string
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set texto
+     *
+     * @param string $texto
+     *
+     * @return Pagina
+     */
+    public function setTexto($texto)
+    {
+        $this->texto = $texto;
+
+        return $this;
+    }
+
+    /**
+     * Get texto
+     *
+     * @return string
+     */
+    public function getTexto()
+    {
+        return $this->texto;
+    }
+
+    /**
+     * Set urlvideo
+     *
+     * @param string $urlvideo
+     *
+     * @return MisionVision
+     */
+    public function setUrlvideo($urlvideo)
+    {
+        $this->urlvideo = $urlvideo;
+
+        return $this;
+    }
+
+    /**
+     * Get urlvideo
+     *
+     * @return string
+     */
+    public function getUrlvideo()
+    {
+        return $this->urlvideo;
+    }
+
+    /**
+     * Set latitud
+     *
+     * @param string $latitud
+     *
+     * @return DondeEstamos
+     */
+    public function setLatitud($latitud)
+    {
+        $this->latitud = $latitud;
+
+        return $this;
+    }
+
+    /**
+     * Get latitud
+     *
+     * @return string
+     */
+    public function getLatitud()
+    {
+        return $this->latitud;
+    }
+
+    /**
+     * Set longitud
+     *
+     * @param string $longitud
+     *
+     * @return DondeEstamos
+     */
+    public function setLongitud($longitud)
+    {
+        $this->longitud = $longitud;
+
+        return $this;
+    }
+
+    /**
+     * Get longitud
+     *
+     * @return string
+     */
+    public function getLongitud()
+    {
+        return $this->longitud;
+    }
+
+    /**
+     * Set direccion
+     *
+     * @param string $direccion
+     *
+     * @return DondeEstamos
+     */
+    public function setDireccion($direccion)
+    {
+        $this->direccion = $direccion;
+
+        return $this;
+    }
+
+    /**
+     * Get direccion
+     *
+     * @return string
+     */
+    public function getDireccion()
+    {
+        return $this->direccion;
+    }
+}
