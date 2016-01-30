89b623f55252beea2926a15abc10b44ba218e7a6
Dioleisys <fontelag@gmail.com>
2016-01-30 14:14:21 -0500

    Widget de Twitter

--- src/BackendBundle/Entity/MiembroConsejo.php
+++ src/BackendBundle/Entity/MiembroConsejo.php
@@ -39,7 +39,7 @@ class MiembroConsejo
     protected $lastname;
 
     /**
-     * @ORM\Column(type="string", length=100)
+     * @ORM\Column(type="string", length=100, nullable=true)
      */
     protected $url_linkedin;
 
@@ -309,6 +309,6 @@ class MiembroConsejo
     }
 
     public function getFullName(){
-        return $this->getName()." ".$this->getFirstname()." ".$this->getLastname();
+        return $this->getName();
     }
 }
